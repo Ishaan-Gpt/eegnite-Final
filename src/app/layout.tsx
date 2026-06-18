447fe72fa5adb447a9160fa6b92a40fc3b0e75f0
Ishaan <ishaangupta011205@gmail.com>
2026-06-18 17:15:13 +0530

    feat: update Apollo Tracker script with new appId

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -140,7 +140,7 @@ export default function RootLayout({
                     dangerouslySetInnerHTML={{
                         __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
 o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
-o.onload=function(){window.trackingFunctions.onLoad({appId:"677a1e61a3f01001b1ebeca8"})},
+o.onload=function(){window.trackingFunctions.onLoad({appId:"698db5e7576e1b000dfc5443"})},
 document.head.appendChild(o)}initApollo();`
                     }}
                 />
